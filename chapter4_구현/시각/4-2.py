5c06935c49bcf1a7dfb707500521528d1417a29e
user.name <user.email>
2022-09-10 21:46:52 +0900

    주석 추가

--- chapter4_구현/시각/4-2.py
+++ chapter4_구현/시각/4-2.py
@@ -4,6 +4,7 @@ count = 0
 for i in range(n + 1):
     for k in range(60):
         for j in range(60):
+            # 문자열들을 합쳐서 그 안에 3이 포함되어 있는지 확인
             if '3' in str(i) + str(j) + str(k):
                 count += 1
 
