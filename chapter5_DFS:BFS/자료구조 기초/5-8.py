744354d467646b42f0ba1739503e667ecefb9fec
pupureum <dlvnfma0501@naver.com>
2022-09-12 21:36:28 +0900

    DFS

---
+++ chapter5_DFS:BFS/자료구조 기초/5-8.py
@@ -0,0 +1,28 @@
+'''
+    Date : 2022년 9월 11일
+    Author : 이푸름
+    Description: 이것이 코딩 테스트다 DFS 예제
+'''
+
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end=' ')
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+
+visited = [False] * 9
+
+dfs(graph, 1, visited)
