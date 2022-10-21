ffeedde4a9f240f7698470d5c5ee7968f4afe33c
pupureum <dlvnfma0501@naver.com>
2022-10-22 00:53:34 +0900

    [ADD] 재귀로 구현한 이진 탐색

---
+++ chapter7_이진탐색/순차 탐색/7-2.py
@@ -0,0 +1,31 @@
+'''
+    Date : 2022년 10월 22일
+    Author : 이푸름
+    Description: 이것이 코딩 테스트다 재귀로 구현한 이진 탐색
+'''
+# 시간 복잡도 : O(logN)
+
+def binary_search(array, target, start, end):
+    if start > end:
+        return None
+    mid = (start + end) // 2
+    # 찾은 경우 중간점 인덱스 반환
+    if array[mid] == target:
+        return mid
+    # 중간점의 값보다 찾고자 하는 값이 작은 경우 왼쪽 확인
+    elif array[mid] > target:
+        return binary_search(array, target, start, mid - 1)
+    # 중간점의 값보다 찾고자 하는 값이 큰 경우 오른쪽 확인
+    else:
+        return binary_search(array, target, mid + 1, end)
+
+# n(원소의 개수)과 target(찾고자 하는 값), 전체 원소 입력 받기
+n, target = list(map(int, input().split()))
+array = list(map(int, input().split()))
+
+# 결과 출력
+result = binary_search(array, target, 0, n - 1)
+if result == None:
+    print("원소가 존재하지 않습니다.")
+else:
+    print(result + 1)
