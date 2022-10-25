29a7ce2abe1dca4e115b3923b3f3a88ae767a58c
pupureum <dlvnfma0501@naver.com>
2022-10-25 23:50:20 +0900

    구조 변경

---
+++ chapter7_이진탐색/7-4.py
@@ -0,0 +1,12 @@
+'''
+    Date : 2022년 10월 25일
+    Author : 이푸름
+    Description: 이것이 코딩 테스트다 한줄 입력받아 출력하는 소스코드
+'''
+
+import sys
+
+# 하나의 문자열 데이터 입력받기
+input_data = sys.stdin.readline().rstrip()
+
+print(input_data)
